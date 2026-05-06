121251d2cdad7999c9cb2bb40bf9f7506ef50dbe
Meet Suthar <meetmewada6655@gmail.com>
2026-05-06 15:34:45 +0530

    fixed

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 # Format: mysql+pymysql://username:password@host:port/dbname
+# DATABASE_URL= "mysql+pymysql://root@localhost:3306/fastapi_db"
 DATABASE_URL = "mysql://root:ZBPvgDoiAOCgjhDZFLfwzMRaafAMKAko@trolley.proxy.rlwy.net:11430/railway"
 
 engine = create_engine(DATABASE_URL)
