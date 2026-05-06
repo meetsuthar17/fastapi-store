18c981fc5a5cfcee2d7746c4a1bfe1fed854ce3f
Meet Suthar <meetmewada6655@gmail.com>
2026-05-06 14:49:04 +0530

    fixed mysql connection and pymysql error

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Format: mysql+pymysql://username:password@host:port/dbname
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/fastapi_db"
+DATABASE_URL = "mysql://root:ZBPvgDoiAOCgjhDZFLfwzMRaafAMKAko@trolley.proxy.rlwy.net:11430/railway"
 
 engine = create_engine(DATABASE_URL)
 
